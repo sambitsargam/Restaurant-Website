728a16ddd28329589e0b15a693ea1305fb6a3685
SAMBIT SARGAM EKALABAYA <sambitsargam2003@gmail.com>
2022-04-22 16:39:51 +0530

    upload the structure

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
+//importamos los comp creados
+import About from './components/about';
+import Product from './components/product';
+import Contact from './components/contact';
+import Home from './components/home';
+import NavBarExample from './layouts/navbar';
+
+function App() {
+  return (
+    <div className="App">
+
+<BrowserRouter>
+<Routes>
+  <Route path='/' element={ <NavBarExample /> }>
+    <Route index element={ <Home /> } />
+    <Route path='about' element={ <About /> } />
+    <Route path='product' element={ <Product /> } />
+    <Route path='contact' element={ <Contact /> } />
+    <Route path='*' element={ <Navigate replace to="/"/> }/>
+  </Route>
+</Routes> 
+</BrowserRouter>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/product.js
@@ -0,0 +1,8 @@
+const Product = () => {
+    return(
+        <div>
+            <h1>View Products</h1>
+        </div>
+    )
+}
+export default Product
