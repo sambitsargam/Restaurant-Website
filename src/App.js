627beadf8a63f7fec0f639176e5eeb005d7ba73d
Nnenna-udefi <miriamudefi@gmail.com>
2022-04-27 08:27:16 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <div className="App">
 
-<BrowserRouter basename={window.location.pathname ||''}>
+<BrowserRouter basename={window.location.pathname || ''}>
 
 <Routes>
   <Route path='/' element={ <NavBarExample /> }>
